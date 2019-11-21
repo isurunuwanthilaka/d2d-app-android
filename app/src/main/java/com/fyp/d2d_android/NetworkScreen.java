74d2fe5d5917106266d97035e34c3e8ca9635434
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2019-11-21 14:28:54 +0530

    Adding message communication with firebase

--- app/src/main/java/com/fyp/d2d_android/NetworkScreen.java
+++ app/src/main/java/com/fyp/d2d_android/NetworkScreen.java
@@ -1,8 +1,5 @@
 package com.fyp.d2d_android;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -22,7 +19,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONObject;
 
@@ -40,6 +46,7 @@ import java.util.Iterator;
 import javax.net.ssl.HttpsURLConnection;
 
 import static android.content.Context.CONNECTIVITY_SERVICE;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class NetworkScreen extends Fragment {
 
@@ -67,7 +74,6 @@ public class NetworkScreen extends Fragment {
         getActivity().registerReceiver(this.myWifiReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
         getActivity().registerReceiver(this.myRssiChangeReceiver, new IntentFilter(WifiManager.RSSI_CHANGED_ACTION));
 
-        final Handler handler = new Handler();
 
         FirebaseAuth mAuth;
         mAuth = FirebaseAuth.getInstance();
@@ -75,6 +81,7 @@ public class NetworkScreen extends Fragment {
         final String userUID =  currentUser.getUid();
 
 
+        final Handler handler = new Handler();
         final Runnable r = new Runnable() {
             public void run() {
 
@@ -84,6 +91,38 @@ public class NetworkScreen extends Fragment {
             }
         };
         handler.postDelayed(r, 60000);
+
+        //firebase messaging setting up
+        FirebaseMessaging.getInstance().subscribeToTopic("News")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = getString(R.string.msg_subscribed);
+                        if (!task.isSuccessful()) {
+                            msg = getString(R.string.msg_subscribe_failed);
+                        }
+                        Log.d(TAG, msg);
+                    }
+                });
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt);
+                        Log.d(TAG, msg);
+                    }
+                });
+        //firebase messaging code ends
 
         return rootView;
     }
