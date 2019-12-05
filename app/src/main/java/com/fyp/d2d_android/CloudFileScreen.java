9e2bcc015165e2a8db2fda30014ce1761491227c
Isuru Nuwanthilaka <31451983+isurunuwanthilaka@users.noreply.github.com>
2019-12-05 22:34:39 +0530

    Send POST request asking for a file.

--- app/src/main/java/com/fyp/d2d_android/CloudFileScreen.java
+++ app/src/main/java/com/fyp/d2d_android/CloudFileScreen.java
@@ -8,15 +8,19 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONObject;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -67,12 +71,35 @@ public class CloudFileScreen extends Fragment {
         for (int i=0;i<fileList.size();i++){
             listView.setItemChecked(i,true);
         }
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //TODO:implement onitemclick logic
+            public void onItemClick(AdapterView<?> arg0, View v, int position,
+                                    long arg3) {
+                String name = arg0.getItemAtPosition(position).toString();
+                Toast.makeText(getContext().getApplicationContext(), "Requesting " + name + ". Wait until pairing.", Toast.LENGTH_LONG).show();
+                requestFileFromCloud(name);
             }
         });
         return view;
+    }
+
+    public void requestFileFromCloud(String name) {
+        // sending post request to the db to get paired
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        final String userUID = currentUser.getUid();
+
+        DataHolder dataHolder = new DataHolder();
+        dataHolder.setUrl("https://us-central1-fyp-cloud-83c3b.cloudfunctions.net/connFileRequest");
+        JSONObject postDataParams = new JSONObject();
+        try {
+            postDataParams.put("deviceID", userUID);
+            postDataParams.put("fileList", name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        dataHolder.setJson(postDataParams);
+        new SendPostRequest().execute(dataHolder);
     }
 }
