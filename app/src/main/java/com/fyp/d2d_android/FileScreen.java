1793e0eec8896f9083679ee3a1df35bb9a4851be
lochana jayawickrama <chathux.lc@gmail.com>
2020-02-15 15:53:55 +0530

    Change app theme

--- app/src/main/java/com/fyp/d2d_android/FileScreen.java
+++ app/src/main/java/com/fyp/d2d_android/FileScreen.java
@@ -10,7 +10,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
