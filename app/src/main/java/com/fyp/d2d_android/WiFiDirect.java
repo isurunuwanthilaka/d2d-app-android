be79ba90c9439987a248afe265e2593f84b7eeb1
lochana jayawickrama <chathux.lc@gmail.com>
2020-02-15 11:40:01 +0530

    Update file upload notification for dynamic file name

--- app/src/main/java/com/fyp/d2d_android/WiFiDirect.java
+++ app/src/main/java/com/fyp/d2d_android/WiFiDirect.java
@@ -324,20 +324,20 @@ public class WiFiDirect extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            createNotification("TestFile.jpg","File upload initiated",android.R.drawable.stat_sys_upload);
+            createNotification(fileName,"File upload initiated",android.R.drawable.stat_sys_upload);
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            createNotification("TestFile.jpg","File upload successful",android.R.drawable.stat_sys_upload_done);
+            createNotification(fileName,"File upload successful",android.R.drawable.stat_sys_upload_done);
             msgFlag = 0;
         }
 
         @Override
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
-            createNotification("TestFile.jpg","File upload in progress",android.R.drawable.stat_sys_upload);
+            createNotification(fileName,"File upload in progress",android.R.drawable.stat_sys_upload);
         }
 
         @Override
